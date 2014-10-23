0b866900d9a722c3b498f70a6e468b350f85741c
Butta <brittany.nelson15@gmail.com>
2014-10-22 22:05:56 -0500

    committing ex's 1-5.

--- exercises/ex1.rb
+++ exercises/ex1.rb
@@ -1,2 +1,11 @@
-Why won't this commit?
-boo!
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this"
+=begin
+puts "This is fun"
+puts "Yay! Printing"
+puts "I'd much rather you 'not'."
+=end
+puts 'I "said" do not touch this.'
+#comments out one line
+#puts "another line."
